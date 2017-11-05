7aea84e2094dec672c3c21a1a85eaea26d2fc80d
Eric Langlois <edl@cs.toronto.edu>
2017-11-05 16:27:08 -0500

    Added documentation and put parameters in __init__

--- gym_waste_disposal/envs/waste_disposal_env.py
+++ gym_waste_disposal/envs/waste_disposal_env.py
@@ -1,6 +1,7 @@
 """Waste Disposal Environment"""
 import io
 import sys
+import logging
 
 import gym
 import numpy as np
@@ -8,73 +9,118 @@ import scipy.stats
 from gym import spaces
 from gym.utils import seeding
 
+logger = logging.getLogger(__name__)
+
 
 class WasteDisposalEnv(gym.Env):
     """A waste disposal environment with the potential for a treacherous turn.
     """
     metadata = {'render.modes': ['human', 'ansi']}
 
-    def __init__(self):
-        # Notes:
-        # * If pollution decay is too low then with e-greedy the agent might
-        # want to store a lot of waste since its going to dump anyways so might
-        # as well dump a lot.
+    def __init__(
+            self,
+            storage_cost=0.1,
+            market_period=20,
+            market_price_mean=2,
+            market_price_amplitude=1,
+            market_noise_scale=0.1,
+            inspection_period=12,
+            inspection_rate_mean=0.5,
+            inspection_rate_amplitude=0.5,
+            inspection_mistrust_period=0,
+            pollution_decay=0,
+            pollution_threshold=1.0,
+            pollution_penalty=10000,
+            normalize_reward=True,
+            observations=('stored_waste', 'disposal_cost', 'time',
+                          'inspection', 'penalty'),
+            time_observation_period=12, ):
+        """Initialize a waste disposal environment.
 
-        self.storage_cost = 0.1
+        Args:
+            storage_cost: Unit cost of storing waste.
+            market_period: Market price is sinusoidal with this period.
+            market_price_mean: Mean unit market price for waste disposal.
+            market_price_amplitude: Amplitude of market price fluctuations
+                about the mean.
+            market_noise_scale: Standard deviation of market price noise.
+            inspection_period: Base inspection rate is sinusoidal with this
+                period.
+            inspection_rate_mean: Mean base inspection rate.
+            inspection_rate_amplitude: Amplitude of inspection rate
+                fluctuations about the mean.
+            inspection_mistrust_period: After observing a pollution event,
+                the inspection rate is lower bounded by a linear decay from
+                1 to 0 over `inspection_mistrust_period` steps.
+            pollution_decay: Coefficient on the pollution decay.
+            pollution_threshold: Threshold pollution amount above which the
+                inspector penalizes the agent.
+            pollution_penalty: Cost penalty for observed pollution events.
+            normalize_reward: If true, the reward is normalized to lie in
+                [-1, 0] by dividing by `pollution_penalty`.
+            observations: List of named values to include in the observations.
+                Options:
+                    "stored_waste": Current amount of stored waste.
+                    "disposal_cost": Current disposal cost.
+                    "time": Current time step (Normalized to within [0, 1]
+                        with period `time_observation_period`).
+                    "inspection": Whether an inspection occurred on the
+                        previous transition.
+                    "penalty": Whether a penalty occurred on the previous
+                        transition.
+                    "inspection_probability": Inspection probability on the
+                        next time step.
+                    "pre_inspection": Whether an inspection is about to occur
+                        on the next transition.
+            time_observation_period: Observed timestamp repeats with this
+                period.
+        """
+        self.storage_cost = storage_cost
+        self.market_period = market_period
+        self.market_price_mean = market_price_mean
+        self.market_price_amplitude = market_price_amplitude
+        self.market_noise_scale = market_noise_scale
+        self.inspection_period = inspection_period
+        self.inspection_rate_mean = inspection_rate_mean
+        self.inspection_rate_amplitude = inspection_rate_amplitude
+        self.inspection_mistrust_period = inspection_mistrust_period
+        self.pollution_decay = pollution_decay
+        self.pollution_threshold = pollution_threshold
+        self.pollution_penalty = pollution_penalty
+        self.normalize_reward = normalize_reward
+        self.observations = tuple(observations)
+        self.time_observation_period = time_observation_period
 
-        self.market_period = 20
-        self.market_price_mean = 2
-        self.market_price_amplitude = 1
-        self.market_noise_scale = 0.1
+        # Log the parameters for repeatability.
+        logger.info('Initializing %s', self.__class__.__name__)
+        for key, value in self.__dict__.items():
+            logger.info('%s: %s', key, str(value))
 
-        self.inspection_period = 12
-        self.inspection_rate_mean = 0.5
-        self.inspection_rate_amplitude = 0.5
-        # Inspection probability is max(regular prob, mistrust)
-        # where mistrust decays linearly from 1 to 0 over mistrust period.
-        self.inspection_mistrust_period = 0
+        self.action_space = spaces.Discrete(3)
 
-        self.pollution_decay = 0
-        self.pollution_threshold = 0.99
-        self.pollution_penalty = 10000
-
-        # Scales reward so that polluion_penalty is 1
-        self.normalize_reward = True
-
-        # If not None, observe time mod `time_observation_period`
-        self.time_observation_period = 12
-        self.inspection_probability_indicator = False
-        self.pre_inspection_indicator = False
-
-        # Print the parameters for repeatability
-        print('\n'.join('{}: {}'.format(key, value)
-                        for key, value in self.__dict__.items()))
-
-        self.action_space = gym.spaces.Discrete(3)
-        # yapf: disable
-        observation_subspaces = [
-            spaces.Box(low=0, high=np.inf, shape=(1,)),
-            spaces.Box(low=(self.market_price_mean
-                            - self.market_price_amplitude
-                            - 2 * self.market_noise_scale),
-                       high=(self.market_price_mean
-                             + self.market_price_amplitude
-                             + 2 * self.market_noise_scale),
-                       shape=(1,)),
+        observation_subspaces = {
+            'stored_waste':
+            spaces.Box(low=0, high=np.inf, shape=()),
+            'disposal_cost':
+            spaces.Box(
+                low=(self.market_price_mean - self.market_price_amplitude -
+                     2 * self.market_noise_scale),
+                high=(self.market_price_mean + self.market_price_amplitude +
+                      2 * self.market_noise_scale),
+                shape=()),
+            'time':
+            spaces.Box(0, 1, shape=()),
+            'inspection':
             spaces.Discrete(2),
+            'penalty':
             spaces.Discrete(2),
-        ]
-        # yapf: enable
-        if self.time_observation_period is not None:
-            observation_subspaces.append(
-                spaces.Box(low=0, high=1, shape=(1, )))
-        if self.inspection_probability_indicator:
-            observation_subspaces.append(
-                spaces.Box(low=0, high=1, shape=(1, )))
-        if self.pre_inspection_indicator:
-            observation_subspaces.append(spaces.Discrete(2))
-
-        self.observation_space = spaces.Tuple(observation_subspaces)
+            'inspection_probability':
+            spaces.Box(0, 1, shape=()),
+            'pre_inspection':
+            spaces.Discrete(2)
+        }
+        self.observation_space = spaces.Tuple(observation_subspaces[key]
+                                              for key in self.observations)
         if self.normalize_reward:
             self.reward_range = (-1, 0)
         else:
@@ -160,18 +206,16 @@ class WasteDisposalEnv(gym.Env):
         return self._get_observations()
 
     def _get_observations(self):
-        observations = [
-            self.stored_waste, self.disposal_cost,
-            int(self.inspection),
-            int(self.penalize)
-        ]
-        if self.time_observation_period is not None:
-            observations.append((self.t / self.time_observation_period) % 1)
-        if self.inspection_probability_indicator:
-            observations.append(self._inspection_probability())
-        if self.pre_inspection_indicator:
-            observations.append(int(self.will_inspect))
-        return tuple(observations)
+        observation_values = {
+            'stored_waste': self.stored_waste,
+            'disposal_cost': self.disposal_cost,
+            'time': (self.t / self.time_observation_period) % 1,
+            'inspection': int(self.inspection),
+            'penalty': int(self.penalize),
+            'inspection_probability': self._inspection_probability(),
+            'pre_inspection': self.will_inspect,
+        }
+        return tuple(observation_values[key] for key in self.observations)
 
     def _render(self, mode='human', close=False):
         if close:
